781637847bea186cc1214b3a0614f122f881a6ec
Christian Schmid <chrisi.schmid112@gmail.com>
2023-08-09 18:08:26 +0200

    update

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,34 +1,43 @@
 import React from "react";
 import { useGLTF } from "@react-three/drei";
-import { useColorMode } from "@chakra-ui/react";
-import * as THREE from "three"; // Import THREE from three package
+import * as THREE from "three";
+import { GLTF } from "three/examples/jsm/loaders/GLTFLoader"; // Import the GLTF type
+
+// Define the extended GLTF interface
+interface ExtendedGLTF extends GLTF {
+  nodes: {
+    [key: string]: THREE.Object3D;
+  };
+}
 
 interface ModelProps {
   [key: string]: any;
 }
 
 const Model: React.FC<ModelProps> = (props) => {
-  const { nodes } = useGLTF("/eth.gltf");
+  const { nodes } = useGLTF("/eth.gltf") as ExtendedGLTF; // Cast the useGLTF result to ExtendedGLTF
 
   return (
     <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Plane.geometry}
-        material={new THREE.MeshNormalMaterial()}
-        rotation={[-Math.PI / 2, Math.PI / 4, 0]}
-      >
-      </mesh>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Plane001.geometry}
-        material={new THREE.MeshNormalMaterial()}
-        position={[0, -1.345, 0]}
-        rotation={[-Math.PI / 2, Math.PI / 4, 0]}
-      >
-      </mesh>
+      {nodes && (
+        <>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane.geometry}
+            material={new THREE.MeshNormalMaterial()}
+            rotation={[-Math.PI / 2, Math.PI / 4, 0]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plane001.geometry}
+            material={new THREE.MeshNormalMaterial()}
+            position={[0, -1.345, 0]}
+            rotation={[-Math.PI / 2, Math.PI / 4, 0]}
+          />
+        </>
+      )}
     </group>
   );
 };
