20e903268a5f761b3340e480e0b73564ec66eb82
chris017 <chrisi.schmid112@gmail.com>
2024-01-12 23:58:32 +0100

    up

--- components/Logos.tsx
+++ components/Logos.tsx
@@ -56,5 +56,31 @@ export const Logos = () => {
         <Image width={100} height={100} alt="Cyberpromote" src="/cp.svg" className="dark:invert"></Image>
         </li>
     </ul>
+    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+        <li>
+        <Image width={100} height={100} alt="avast" src="/avast.svg" className="dark:invert"></Image>
+        </li>
+        <li>
+        <Image width={100} height={100} alt="g+d" src="/gd.svg" className="dark:invert"></Image>
+        </li>
+        <li>
+        <Image width={100} height={100} alt="Telefonica" src="/tef.svg" className="dark:invert"></Image>
+        </li>
+        <li>
+        <Image width={100} height={100} alt="Lobeco" src="/lobeco.svg" className="dark:invert"></Image>
+        </li>
+        <li>
+        <Image width={100} height={100} alt="Sparkasse" src="/ssk.svg" className="dark:invert"></Image>
+        </li>
+        <li>
+        <Image width={100} height={100} alt="Bauhaus" src="/bau.svg" className="dark:invert"></Image>
+        </li>
+        <li>
+        <Image width={100} height={100} alt="Hoffmann" src="/hg.svg" className="dark:invert"></Image>
+        </li>
+        <li>
+        <Image width={100} height={100} alt="Cyberpromote" src="/cp.svg" className="dark:invert"></Image>
+        </li>
+    </ul>
 </div>
     )}
